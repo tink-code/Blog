270ca7a803efdd66b597293b27b90ea7e27f0a9f
Akhil <Akhi1@users.noreply.github.com>
2018-08-01 22:58:41 +0530

    Create DecryptingDES.c

---
+++ Computer Networks/DecryptingDES.c
@@ -0,0 +1,39 @@
+/* 
+Take a 64 bit Plain text and encrypt the same using DES algorithm. (refer EncrytionDES.c)
+Now, write a program to break the above DES coding.
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<ctype.h>
+
+void main()
+{
+	char pwd[20];
+	char alpha[26]="abcdefghijklmnopqrstuvwxyz";
+	int num[20],i,n,key;
+	//clrscr();
+	printf("\nEnter the password:");
+	scanf("%s",&pwd);
+	n=strlen(pwd);
+	for(i=0;i<n;i++)
+		num[i]=toascii(tolower(pwd[i]))-'a';
+	printf("\nEnter the key:");
+	scanf("%d",&key);
+	for(i=0;i<n;i++)
+		num[i]=(num[i]+key)%26;
+	for(i=0;i<n;i++)
+		pwd[i]=alpha[num[i]];
+	printf("\nThe key is:%d",key);
+	printf("\nEncrypted text is:%s",pwd);
+	for(i=0;i<n;i++)
+	{
+		num[i]=(num[i]-key)%26;
+		if(num[i]<0)
+			num[i]=26+num[i];
+		pwd[i]=alpha[num[i]];
+	}
+	printf("\nDecrypted text is:%s",pwd);
+	getch();
+}
