2b397e7fcfe82f5a2a1dc9a0a57084771b6d72c6
Akhil <Akhi1@users.noreply.github.com>
2018-12-29 11:36:01 +0530

    Create RSA.java

---
+++ Cryptography and Network Security/Programs/RSA.java
@@ -0,0 +1,35 @@
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.Scanner;
+
+public class RSA {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+    System.out.print("Enter a Prime number: ");
+    BigInteger p = sc.nextBigInteger();
+    System.out.print("Enter another prime number: ");
+    BigInteger q = sc.nextBigInteger();
+    BigInteger n = p.multiply(q);
+    BigInteger n2 = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+    BigInteger e = generateE(n2);
+    BigInteger d = e.modInverse(n2);
+    System.out.println("Encryption keys are: " + e + ", " + n);
+    System.out.println("Decryption keys are: " + d + ", " + n);
+}
+
+public static BigInteger generateE(BigInteger fiofn) {
+int y, intGCD;
+BigInteger e;
+BigInteger gcd;
+Random x = new Random();
+do {
+y = x.nextInt(fiofn.intValue()-1);
+String z = Integer.toString(y);
+e = new BigInteger(z);
+gcd = fiofn.gcd(e);
+intGCD = gcd.intValue();
+}
+while(y <= 2 || intGCD != 1);
+return e;
+}
+}
