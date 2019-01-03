173a06c4e1dc6e33430e4c939ce73a56dd83ad11
Akhil <Akhi1@users.noreply.github.com>
2019-01-03 12:50:11 +0530

    Update and rename XOR.c to Xor.c

--- Cryptography and Network Security/Programs/Xor.c
+++ Cryptography and Network Security/Programs/Xor.c
@@ -1,12 +1,13 @@
 #include<stdlib.h>
 void main(){
     char str[]="Hello World";
-    char str1[11];
     int i,len;
     len=strlen(str);
     for(i=0;i<len;i++){
         str1[i]=str[i]^0;
+    }
+    for(i=0;i<len;i++){
         printf("%c",str1[i]);
     }
-    printf("\n");
+   getch();
 }
