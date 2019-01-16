5fecbc7b3fdf23cd0b31fd432fc7d124870180c1
Akhil <Akhi1@users.noreply.github.com>
2019-01-16 18:28:40 +0530

    Update DiffieHellman.java

--- Cryptography and Network Security/Programs/DiffieHellman.java
+++ Cryptography and Network Security/Programs/DiffieHellman.java
@@ -5,44 +5,43 @@ import java.security.KeyPairGenerator;
 import java.security.SecureRandom; 
 import javax.crypto.spec.DHParameterSpec; 
 import javax.crypto.spec.DHPublicKeySpec; 
-public class DiffeHellman { 
+
+public class DiffieHellman { 
 public final static int pValue = 47; 
 public final static int gValue = 71; 
 public final static int XaValue = 9; 
 public final static int XbValue = 14; 
-public static void main(String[] args) throws Exception { 
-        // TODO code application logic here 
-BigInteger p = new BigInteger(Integer.toString(pValue)); 
-BigInteger g = new BigInteger(Integer.toString(gValue)); 
-BigIntegerXa = new BigInteger(Integer.toString(XaValue)); 
-BigIntegerXb = new BigInteger(Integer.toString(XbValue)); 
-createKey(); 
-intbitLength = 512; // 512 bits 
-SecureRandomrnd = new SecureRandom(); 
-        p = BigInteger.probablePrime(bitLength, rnd); 
-        g = BigInteger.probablePrime(bitLength, rnd); 
-createSpecificKey(p, g); 
-    } 
-public static void createKey() throws Exception { 
-KeyPairGeneratorkpg = KeyPairGenerator.getInstance("DiffieHellman"); 
-kpg.initialize(512); 
-KeyPairkp = kpg.generateKeyPair(); 
-KeyFactorykfactory = KeyFactory
-.getInstance("DiffieHellman"); 
-DHPublicKeySpeckspec  =  (DHPublicKeySpec)  kfactory.getKeySpec(kp.getPublic(),  
-DHPublicKeySpec.class); 
-System.out.println("Public key is: " +kspec); 
-    } 
-public static void createSpecificKey(BigInte
-ger p, BigInteger g) throws Exception { 
-KeyPairGeneratorkpg = KeyPairGenerator.getInstance("DiffieHellman"); 
-DHParameterSpecparam = new DHParameterSpec(p, g); 
-kpg.initialize(param); 
-KeyPairkp = kpg.generateKeyPair(); 
-KeyFactorykfactory = KeyFactory
-.getInstance("DiffieHellman"); 
-DHPublicKeySpeckspec  =  (DHPublicKeySpec)  kfactory.getKeySpec(kp.getPublic(),  
-DHPublicKeySpec.class); 
-System.out.println("\nPublic key is : " +kspec); 
-    } 
+  public static void main(String[] args) throws Exception { 
+      // TODO code application logic here 
+    BigInteger p = new BigInteger(Integer.toString(pValue)); 
+    BigInteger g = new BigInteger(Integer.toString(gValue)); 
+    BigInteger Xa = new BigInteger(Integer.toString(XaValue)); 
+    BigInteger Xb = new BigInteger(Integer.toString(XbValue)); 
+    createKey(); 
+    int bitLength = 512; // 512 bits 
+    SecureRandom rnd = new SecureRandom(); 
+    p = BigInteger.probablePrime(bitLength, rnd); 
+    g = BigInteger.probablePrime(bitLength, rnd); 
+    createSpecificKey(p, g); 
+  } 
+  
+  public static void createKey() throws Exception { 
+    KeyPairGenerator kpg = KeyPairGenerator.getInstance("DiffieHellman"); 
+    kpg.initialize(512); 
+    KeyPair kp = kpg.generateKeyPair(); 
+    KeyFactory kfactory = KeyFactory.getInstance("DiffieHellman"); 
+    DHPublicKeySpec kspec  =  (DHPublicKeySpec)  kfactory.getKeySpec(kp.getPublic(), DHPublicKeySpec.class); 
+    System.out.println("Public key is: " +kspec); 
+  } 
+
+  public static void createSpecificKey(BigInteger p, BigInteger g) throws Exception { 
+    KeyPairGenerator kpg = KeyPairGenerator.getInstance("DiffieHellman"); 
+    DHParameterSpec param = new DHParameterSpec(p, g); 
+    kpg.initialize(param); 
+    KeyPair kp = kpg.generateKeyPair(); 
+    KeyFactory kfactory = KeyFactory.getInstance("DiffieHellman"); 
+    DHPublicKeySpec kspec  =  (DHPublicKeySpec)  kfactory.getKeySpec(kp.getPublic(), DHPublicKeySpec.class); 
+    System.out.println("\nPublic key is : " +kspec); 
+  }
+  
 } 
