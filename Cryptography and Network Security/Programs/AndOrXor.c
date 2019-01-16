ac892a3bd3f6d870783624691f12358839379906
Akhil <Akhi1@users.noreply.github.com>
2019-01-16 15:09:44 +0530

    Update AndOrXor.c

--- Cryptography and Network Security/Programs/AndOrXor.c
+++ Cryptography and Network Security/Programs/AndOrXor.c
@@ -14,17 +14,17 @@ void main(){
 		XOR[i]=str1[i]^0;
 	}
 
-	printf("\nPerforming AND Operation\n");
+	printf("\nAfter Performing AND Operation\n");
 	for(i=0; i<len; i++){
 		printf("%c", AND[i]);
 	}
 
-	printf("\n\n\nPerforming OR Operation\n");
+	printf("\n\n\nAfter Performing OR Operation\n");
 	for(i=0; i<len; i++){
 		printf("%c",OR[i]);
 	}
 
-	printf("\n\n\nPerformaing XOR Operation\n");
+	printf("\n\n\nAfter Performaing XOR Operation\n");
 	for(i=0; i<len; i++){
 		printf("%c", XOR[i]);
 	}
